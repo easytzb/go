983f90556b3e647114f20d12cd84f1030dbde54e
easytzb <easytzb@gmail.com>
2016-09-05 09:26:41 +0800

    .

--- checkZtjUpdate/checkZtjUpdate.go
+++ checkZtjUpdate/checkZtjUpdate.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
@@ -13,32 +13,32 @@ func main() {
 	//r, err := http.Get("http://m.qidian.com/book/showbook.aspx?bookid=3347595")
 	r, err := http.Get("http://www.qidian.com/Book/3347595.aspx")
 	if err != nil {
-		fmt.Println("获取网页内容失败:1", err.Error())
+		log.Println("获取网页内容失败:1", err)
 		return
 	}
 
 	body, err := ioutil.ReadAll(r.Body)
 	r.Body.Close()
 	if err != nil {
-		fmt.Println("获取网页内容失败:2", err.Error())
+		log.Println("获取网页内容失败:2", err)
 		return
 	}
 
-	//fmt.Println(string(body[:]))
+	//log.Println(string(body[:]))
 	//return
 
 	//date := regexp.MustCompile(`\d{4}(/|-)\d{1,2}(/|-)\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}`).FindString(string(body[:]))
 	date := regexp.MustCompile(`dateModified">(\d{4}-\d\d-\d\d \d\d:\d\d)<\/span>`).FindSubmatch(body)
 	if len(date) == 0 {
-		fmt.Println("解析更新日期失败:3")
+		log.Println("解析更新日期失败:3")
 		return
 	} else {
-		//fmt.Println(date)
+		//log.Println(date)
 	}
 
 	fr, err := os.Open("/webser/logs/ztj.date")
 	if err != nil {
-		fmt.Println("日期记录文件打开失败:4", err.Error())
+		log.Println("日期记录文件打开失败:4", err)
 		return
 	}
 	defer fr.Close()
@@ -46,7 +46,7 @@ func main() {
 	oldDate := make([]byte, 16)
 	count, _ := fr.Read(oldDate)
 	//if err != nil {
-	//	fmt.Println("原更新时间读取失败:5", err.Error())
+	//	log.Println("原更新时间读取失败:5", err)
 	//	return
 	//}
 
@@ -54,13 +54,13 @@ func main() {
 		//新的更新时间写入文件
 		fw, er := os.Create("/webser/logs/ztj.date")
 		if er != nil {
-			fmt.Println("日期记录文件打开失败:7", err.Error())
+			log.Println("日期记录文件打开失败:7", err)
 			return
 		}
 		defer fw.Close()
 
 		if _, err := fw.Write(date[1]); err != nil {
-			fmt.Println("更新时间写入失败:6", err.Error())
+			log.Println("更新时间写入失败:6", err)
 			return
 		}
 
@@ -68,7 +68,7 @@ func main() {
 		cmd := exec.Command("/usr/bin/php", "/webser/www/tchat/cron/index.php", "sendMsg2Me", "maoni updated!!!")
 		err = cmd.Run()
 		if err != nil {
-			fmt.Println("微信信息发送失败:6", err.Error())
+			log.Println("微信信息发送失败:6", err)
 		}
 
 	}
